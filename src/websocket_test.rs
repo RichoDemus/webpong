fc5007c0f4224506af7fa88c930e3bdacbfb4a7d
RichoDemus <git@richodemus.com>
2021-03-07 22:16:13 +0100

    fix ws test

--- src/websocket_test.rs
+++ src/websocket_test.rs
@@ -9,7 +9,7 @@ mod tests {
     #[tokio::test(flavor = "multi_thread", worker_threads = 10)]
     async fn my_test() -> Result<(), Box<dyn std::error::Error>> {
         let _ = env_logger::builder()
-            .filter_module("webpong", log::LevelFilter::Info)
+            .filter_module("webpong", log::LevelFilter::Trace)
             .try_init();
 
         let ws_server = ws_server::WebsocketServer::start();
@@ -20,31 +20,34 @@ mod tests {
             tokio::join!(ws_server, ws_client_one, ws_client_two,);
 
         let mut server = server?;
-        let server_running = server.running.clone();
+        let server_running_for_stopping = server.running.clone();
+        let server_running_for_listen_loop = server.running.clone();
         tokio::spawn(async move {
-            while let Some(mut ws_client) = server.event_stream.next_event().await {
-                tokio::spawn(async move {
-                    info!("Listening to messages from client");
-                    loop {
-                        if let Some(msg) = ws_client.event_stream.next_event().await {
-                            match msg {
-                                WsEvent::Message(msg) => {
-                                    let msg = msg.as_str();
-                                    info!("Server received msg: {:?}", msg);
-                                    ws_client.send(msg).await;
-                                    tokio::time::sleep(Duration::from_millis(100)).await;
+            while *server_running_for_listen_loop.lock().expect("loop") {
+                while let Some(mut ws_client) = server.event_stream.next_event().await {
+                    tokio::spawn(async move {
+                        info!("Listening to messages from client");
+                        loop {
+                            if let Some(msg) = ws_client.event_stream.next_event().await {
+                                match msg {
+                                    WsEvent::Message(msg) => {
+                                        let msg = msg.as_str();
+                                        info!("Server received msg: {:?}", msg);
+                                        ws_client.send(msg).await;
+                                        break;
+                                    }
+                                    m => warn!("Server received unexpected msg: {:?}", m),
                                 }
-                                WsEvent::Closed => break,
-                                m => warn!("Server received unexpected msg: {:?}", m),
+                                tokio::time::sleep(Duration::from_millis(1)).await;
                             }
-                            tokio::time::sleep(Duration::from_millis(100)).await;
                         }
-                    }
-                    info!("Server listening loop ending...");
-                });
-                tokio::time::sleep(Duration::from_millis(100)).await;
+                        trace!("Server listening loop ended...");
+                    });
+                    tokio::time::sleep(Duration::from_millis(1)).await;
+                }
             }
             server.close().await;
+            trace!("Server task stopped");
         });
 
         let client_futures = vec![client_one, client_two]
@@ -64,10 +67,11 @@ mod tests {
                                 WsEvent::Closed => {}
                             },
                         }
-                        tokio::time::sleep(Duration::from_millis(100)).await;
+                        tokio::time::sleep(Duration::from_millis(1)).await;
                     }
                     info!("Client listening loop ending...");
                     ws.close().await;
+                    trace!("Client listening loop ended");
                 })
             })
             .collect::<Vec<_>>();
@@ -76,8 +80,10 @@ mod tests {
         for future in client_futures {
             future.await.expect("future await failed");
         }
+        info!("futures done");
 
-        *server_running.lock().expect("close server") = false;
+        *server_running_for_stopping.lock().expect("close server") = false;
+        info!("Main thread ending, both clients are done");
         Ok(())
     }
 }
